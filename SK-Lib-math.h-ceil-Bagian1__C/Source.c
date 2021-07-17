1f980fbcbb1d61fb00a6a1ad947e15cc10dabd19
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-17 12:10:23 +0700

    Add project files.

---
+++ SK-Lib-math.h-ceil-Bagian1__C/Source.c
@@ -0,0 +1,12 @@
+#include <stdio.h>      /* printf */
+#include <math.h>       /* ceil */
+#include <conio.h>
+
+int main() {
+	printf("ceil of 2.3 is %.1f\n", ceil(8.3));
+	printf("ceil of 3.8 is %.1f\n", ceil(1.2));
+	printf("ceil of -2.3 is %.1f\n", ceil(-5.4));
+	printf("ceil of -3.8 is %.1f\n", ceil(-1.9));
+	_getch();
+	return 0;
+}
